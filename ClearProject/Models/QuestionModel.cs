cc7bad615694ba6e68ed0161bd1d0cee83bdb11e
KoAmar <43239620+KoAmar@users.noreply.github.com>
2019-10-29 04:17:06 +0300

    clear mvc

---
+++ ClearProject/Models/QuestionModel.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace ClearProject.Models
+{
+    public class QuestionModel
+    {
+        public string Question { get; set; }
+        public bool SoleAnswer { get; set; }
+        public List<string> PossibleAnswers { get; set; }
+    }
+}
