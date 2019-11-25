8eb420934f95a1307ddf79a15a93783fa056332e
KoAmar <43239620+KoAmar@users.noreply.github.com>
2019-11-25 04:10:17 +0300

    entity framework attached

---
+++ MainMVC/Models/Polls/Answer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MainMVC.Models.Polls
+{
+    public class Answer
+    {
+        public Answer(string text, int answerSelectedCounter = 0)
+        {
+            Text = text;
+            AnswerSelectedCounter = answerSelectedCounter;
+        }
+
+        public int Id { get; set; }
+        public string Text { get; set; }
+        public int AnswerSelectedCounter { get; set; }
+    }
+}
---
+++ MainMVC/Models/Polls/SQLPollRerository.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MainMVC.Models.Polls
+{
+    public class SQLPollRerository : IPollRepository
+    {
+        private readonly AppDbContext context;
+
+        public SQLPollRerository(AppDbContext context)
+        {
+            this.context = context;
+        }
+
+        public Poll Add(Poll poll)
+        {
+            context.Polls.Add(poll);
+            context.SaveChanges();
+            return poll;
+        }
+
+        public Poll Delete(int Id)
+        {
+            Poll poll = context.Polls.Find(Id);
+            if (poll != null)
+            {
+                context.Polls.Remove(poll);
+                context.SaveChanges();
+            }
+            return poll;
+        }
+
+        public IEnumerable<Poll> GetPolls()
+        {
+            return context.Polls;
+        }
+
+        public Poll GetPoll(int Id)
+        {
+            return context.Polls.Find(Id);
+        }
+
+        public Poll Update(Poll pollChanges)
+        {
+            var poll = context.Polls.Attach(pollChanges);
+            poll.State = Microsoft.EntityFrameworkCore.EntityState.Modified;
+            context.SaveChanges();
+            return pollChanges;
+        }
+    }
+}
